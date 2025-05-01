ac893e1c2bd31da8bab07adee853bea32f171458
Muhammad Arslan Talib <129850990+arslan7676@users.noreply.github.com>
2025-05-01 14:08:56 -0700

    updated code

--- src/app/careers/career-details/page.tsx
+++ src/app/careers/career-details/page.tsx
@@ -5,11 +5,9 @@ import Navbar from "@/components/Layout/Navbar";
 export default function CareerDetailsPage() {
   return (
     <>
-      <div className="bg-[#477ef5]">
         <Navbar />
-
         <hr className="border-[#E3E3E3]" />
-
+      <div className="bg-[#477ef5]">
         <CareerDetailsContent />
       </div>
 
