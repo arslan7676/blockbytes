2d2a894d400c9cc20d4fc0261a3ed5df5e1c360f
Muhammad Arslan Talib <129850990+arslan7676@users.noreply.github.com>
2025-05-01 14:04:47 -0700

    updated cde

--- src/app/success-stories/page.tsx
+++ src/app/success-stories/page.tsx
@@ -2,7 +2,7 @@ import BorderLine from "@/components/Common/BorderLine";
 import BrandsSlider from "@/components/Common/BrandsSlider";
 import TestimonialsSliderThree from "@/components/Common/TestimonialsSliderThree";
 import Footer from "@/components/Layout/Footer";
-import OurProcess from "@/components/SuccessStories/OurProcess";
+import OurProcess from "@/components/Home/TraditionalOfflineAdAgency/OurProcess";
 import SuccessStories from "@/components/SuccessStories/SuccessStories";
 import SuccessStoriesHeader from "@/components/SuccessStories/SuccessStoriesHeader";
 import Navbar from "@/components/Layout/Navbar";
